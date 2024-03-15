71625089c14f532a4313ae853c6ec42087b25093
sharon simpson <sharon.m.simpson@icloud.com>
2024-03-15 01:18:25 +0000

    created search bar with js in articles

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+
+let select = document.getElementById("select");
+let list = document.getElementById("list");
+
+select.onclick = function() {
+    list.classList.toggle("open");
+}
