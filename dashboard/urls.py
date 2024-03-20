88e5303f4cc720d7715f93138f77379aec3eb5eb
sharon simpson <sharon.m.simpson@icloud.com>
2024-03-20 08:09:02 +0000

    Start tidy up articles add and Dash app

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
@@ -6,4 +6,8 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('article/', include('article.urls')),
+    path('mainhub/',include('mainhub.urls')),
+    path('mainhub/', include('django.contrib.auth.urls')),
+
 ]
